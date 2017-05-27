f8828dd738390928a7618529c3982520e7ec58f4
Paul Yarabe <pyarabe@mba2018.hbs.edu>
2017-05-27 19:19:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(int)
+  if int <=1
+    return false
+  end
+  arr = (2...int).to_a
+  arr.each do |number|
+    if int % number == 0
+      return false
+    end
+  end
+  return true
+end
